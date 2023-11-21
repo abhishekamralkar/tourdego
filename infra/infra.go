4bc0921ae4102f954b9459516df2928e49e41b29
Abhishek Amralkar <abhishekamralkar@gmail.com>
2023-11-21 07:45:27 -0800

    [tourdego] rename cloud to infra

--- infra/infra.go
+++ infra/infra.go
@@ -1,9 +1,15 @@
 package main
 
 type Instance struct {
-	Name      string
-	PrivateIP string
-	OS        string
+	Name      string `json:"name"`
+	PrivateIP string `json:"privateip"`
+	OS        string `json:"os"`
+	Tags      Tags   `json:"tags"`
+}
+
+type Tags struct {
+	Service string
+	Project string
 }
 
 func getEc2() (instances []Instance) {
@@ -12,12 +18,20 @@ func getEc2() (instances []Instance) {
 		Name:      "instance-1",
 		PrivateIP: "192.168.2.1",
 		OS:        "Linux",
+		Tags: Tags{
+			Service: "Web",
+			Project: "E-Commerce",
+		},
 	}
 
 	instance2 := Instance{
 		Name:      "instance-2",
 		PrivateIP: "192.168.2.2",
 		OS:        "Linux",
+		Tags: Tags{
+			Service: "Web",
+			Project: "E-Commerce",
+		},
 	}
 
 	instances = append(instances, instance1)
